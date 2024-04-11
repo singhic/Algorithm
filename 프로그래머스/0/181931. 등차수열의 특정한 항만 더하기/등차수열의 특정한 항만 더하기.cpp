165468e127e4174e5e68898b3ac30201698c1a29
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2024-04-11 13:08:40 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int d, vector<bool> included) {
+    int answer = 0;
+    for(int i = 0; i < included.size(); i++)
+    {
+        if(included[i])
+        {
+            answer += d * i + a;
+        }
+    }
+    return answer;
+}
