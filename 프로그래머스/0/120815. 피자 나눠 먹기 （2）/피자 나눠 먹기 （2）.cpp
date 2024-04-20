1bd841400e434e5f734cac145e481ed78d571b14
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2024-04-21 00:00:04 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 1; i <= 100; i++)
+    {
+        if((i * 6) % n == 0)
+        {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
