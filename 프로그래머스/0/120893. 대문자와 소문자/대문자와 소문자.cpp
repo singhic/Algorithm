70c5064e30ab049c03b710cb63bc56482c1e9da4
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2024-04-30 15:29:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    for(auto& v : my_string)
+    {
+        if(islower(v))
+        {
+            v = toupper(v);
+        }
+        else 
+        {
+            v = tolower(v);
+        }
+    }
+    return my_string;
+}
