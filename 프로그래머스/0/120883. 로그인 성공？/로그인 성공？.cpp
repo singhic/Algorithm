7ebfb47e77133bf59b294702f18b91ee4419e7ac
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2024-05-20 12:02:31 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "";
+    
+    for(int i = 0; i < db.size(); i++)
+    {
+        if(db[i][0] == id_pw[0])
+        {
+            if(db[i][1] == id_pw[1])
+            {
+                answer = "login";
+                break;
+            }
+            answer = "wrong pw";
+            break;
+        }
+    
+        if (i == db.size() - 1)
+        {
+            answer = "fail";
+            break;
+        }
+    }
+    return answer;
+}
