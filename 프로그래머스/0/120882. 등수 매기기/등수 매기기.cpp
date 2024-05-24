c7e8c17c92aab2d96f8ba18c784566831e33d899
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2024-05-24 16:14:24 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> score) {
+    vector<int> answer;
+    vector<int> v;
+    
+    for(int i = 0; i < score.size(); i++)
+        v.push_back(score[i][0]+score[i][1]);
+    
+    for(int i = 0; i < v.size(); i++)
+    {
+    	int cnt = 1;
+        for(int j = 0; j < v.size(); j++)
+        	if(v[i] < v[j]) cnt++;
+        
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
